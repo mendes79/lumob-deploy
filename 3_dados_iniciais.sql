437ae7efbd633d925868aa34f3979a8ab79da6f6
Mendes79 <mendes79@gmail.com>
2025-08-26 19:00:42 -0300

    (feat): Script p/ povoar tabela usuarios

---
+++ 3_dados_iniciais.sql
@@ -0,0 +1,8 @@
+-- Arquivo: 3_dados_iniciais.sql
+
+-- Insere os módulos para a lógica de permissão do admin funcionar
+INSERT INTO `modulos` (`ID_Modulo`, `Nome_Modulo`) VALUES
+(1, 'Pessoal'),
+(2, 'Obras'),
+(3, 'Usuários'),
+(4, 'Segurança');
